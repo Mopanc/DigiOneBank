baa342bd741585056b21fdec8c6a07180fe9cd95
mopanc <jorgemopanc@icloud.com>
2021-07-08 10:15:21 -0300

    create project kotlin studies and first exercise INTARRAY

---
+++ src/main/kotlin/digitalInovationOne/testintarray.kt
@@ -0,0 +1,31 @@
+package digitalInovationOne
+
+fun main() {
+    val values = IntArray(5)
+    values[0] = 5
+    values[1] = 4
+    values[2] = 3
+    values[3] = 2
+    values[4] = 1
+
+    for (valor in values) {
+        println(valor)
+    }
+
+    println("----------------")
+    values.forEach { valor ->
+        println(valor)
+    }
+
+    println("----------------")
+    for (index in values.indices) {
+        println(values[index])
+    }
+
+    println("----------------")
+    values.sort()
+    for (valor in values) {
+        println(valor)
+    }
+
+}
