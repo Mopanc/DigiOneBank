7437341201c3da5a1f313bb0d2dd0e8b7bc5b8c0
mopanc <jorgemopanc@icloud.com>
2021-07-09 13:48:42 -0300

    add sortedby and groupby

--- src/main/kotlin/digitalInovationOne/testlist.kt
+++ src/main/kotlin/digitalInovationOne/testlist.kt
@@ -1,10 +1,10 @@
 package digitalInovationOne
 
 fun main() {
-    val jorge = Funcionario("Jorge", 1000.0)
-    val gabriella = Funcionario("Gabriella", 2000.0)
-    val matheus = Funcionario("Matheus", 4000.0)
-    val beatriz = Funcionario("Beatriz", 4000.0)
+    val jorge = Funcionario("Jorge", 3000.0,"clt")
+    val gabriella = Funcionario("Gabriella", 2000.0,"pj")
+    val matheus = Funcionario("Matheus", 4000.0,"pj")
+    val beatriz = Funcionario("Beatriz", 5000.0,"clt")
 
     val funcionarios = listOf(jorge, gabriella, matheus, beatriz)
 
@@ -13,11 +13,22 @@ fun main() {
     println("-----------")
     println(funcionarios.find { it.nome == "Matheus" })
 
+    println("-----------")
+    funcionarios
+        .sortedBy { it.salario }
+        .forEach { println(it) }
+
+    println("-----------")
+    funcionarios
+        .groupBy { it.tipoContratacao }
+        .forEach { println(it) }
+
 }
 
 data class Funcionario(
     val nome: String,
-    val salario: Double
+    val salario: Double,
+    val tipoContratacao: String
 ) {
     override fun toString(): String =
     """
